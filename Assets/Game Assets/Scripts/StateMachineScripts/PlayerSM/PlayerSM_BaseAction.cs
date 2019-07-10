f40cf5ca6e23574d6fab20c2f3ed52fbd94b2d9e
Lorién <lorienportella@gmail.com>
2019-07-10 20:40:53 +0200

    more player controller chanfes

--- Assets/Game Assets/Scripts/StateMachineScripts/PlayerSM/PlayerSM_BaseAction.cs
+++ Assets/Game Assets/Scripts/StateMachineScripts/PlayerSM/PlayerSM_BaseAction.cs
@@ -6,13 +6,55 @@ namespace NodeCanvas.Tasks.Actions{
 
 	public class PlayerSM_BaseAction : ActionTask{
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected PlayerController player_script;
+
+       protected void initializeBaseTask()
+        {
+            player_script = agent.GetComponent<PlayerController>();
+        }
+
+        protected override string OnInit()
+        {
+            initializeBaseTask();
+            return null;
+        }
+
         protected virtual void HandleInput()
         { }
          protected virtual void ComputeVelocity()
         { }
         protected virtual void HandleAnimations()
         { }
-	}
+        protected virtual void SpecificOnInit()
+        {
+
+        }
+
+        protected override void OnExecute()
+        {
+            EndAction(true);
+        }
+
+        //Called once per frame while the action is active.
+        protected override void OnUpdate()
+        {
+            HandleInput();
+            ComputeVelocity();
+            HandleAnimations();
+        }
+
+        //Called when the task is disabled.
+        protected override void OnStop()
+        {
+
+        }
+
+        //Called when the task is paused.
+        protected override void OnPause()
+        {
+
+        }
+    }
 }
